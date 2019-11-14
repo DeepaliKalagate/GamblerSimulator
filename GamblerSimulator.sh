8fcd7b5775ebd17c8e52bcb2fe52fb85e7fb0ae3
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-14 12:53:10 +0530

    Calculated fifty percent of the initial stake and resign for the day

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -1,19 +1,35 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 echo "Welcome in Gambler Game"
 
 STAKE=100
 BET=1
 win=0
+winLimit=0
+loseLimit=0
+stakeOfGame=$STAKE
+
+stake=$(( ($stakeOfGame * 50) / 100 ))
+loseLimit=$(($stakeOfGame - $stake ))
+winLimit=$(( $stakeOfGame + $stake ))
+
 
 function gamble()
 {
-        bet=$((RANDOM%2))
-        if [ $bet -eq $win ]
-        then
-                STAKE=$(( $STAKE+1 ))
-        else
-                STAKE=$(( $STAKE-1 ))
-        fi
+	while [[ true ]]
+	do
+		if [[ $stakeOfGame -gt $loseLimit ]] && [[ $stakeOfGame -lt $winLimit ]]
+		then
+			break
+      bet=$((RANDOM%2))
+      	if [ $bet -eq $win ]
+        	then
+               		STAKE=$(( $STAKE+1 ))
+			break
+        	else
+                	STAKE=$(( $STAKE-1 ))
+        	fi
+		fi
+	done
 }
 gamble
 
