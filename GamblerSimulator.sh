f5d8dcb2efde4f8ed7082f08aa673811a9c8f0ba
KumudGarg <kumud50garg@gmail.com>
2019-11-14 11:42:27 +0530

    Checked either win or lose

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -3,4 +3,17 @@ echo "Welcome in Gambler Game"
 
 STAKE=100
 BET=1
+win=0
+
+function gamble()
+{
+        bet=$((RANDOM%2))
+        if [ $bet -eq $win ]
+        then
+                STAKE=$(( $STAKE+1 ))
+        else
+                STAKE=$(( $STAKE-1 ))
+        fi
+}
+gamble
 
