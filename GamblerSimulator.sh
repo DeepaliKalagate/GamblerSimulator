246db7fd042507846fd11eb0ef6935b1612272d2
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-15 11:50:18 +0530

    Refactored code and found the lucky or unlucky day

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -2,18 +2,23 @@
 echo "Welcome in Gambler Game"
 
 declare -A daywiseAmountTrack
-
+declare -A daywiseGrowthTrack
 declare BET_AMOUNT=1;
 declare WIN=1;
 declare DAYS=20;
 
 declare stake=100;
-declare maxAllowance=$(( $stake+ (( $stake /2 )) )); 
+declare maxAllowance=$(( $stake+ (( $stake /2 )) ));
 declare leastAllowance=$(( $stake/2 ))
 declare totalAmount=0;
 declare initialDayStake=100;
 declare dayslost=0;
 declare dayswon=0;
+declare unluckyday=0
+declare luckyday=0;
+declare maxAmount=-10000;
+declare leastAmount=10000;
+declare counter=0
 
 function bet()
 {
@@ -35,18 +40,25 @@ function calculativeGame()
 }
 function daywiseDictionary()
 {
-
+	
 	if [ $stake -eq $leastAllowance ]
-          then
+	then
           daywiseAmountTrack[$day]=$(($stake-$initialDayStake))
-          totalAmount=$(($totalAmount-stake))
+				totalAmount=$(($totalAmount-stake))
+
+			daywiseGrowthTrack[$counter]=$totalAmount
+			counter=$(($counter+1))
 			else
           daywiseAmountTrack[$day]=$(($stake-$initialDayStake))
           totalAmount=$(($totalAmount+$stake-$initialDayStake))
+			 daywiseGrowthTrack[$(($counter+1))]=$totalAmount
+			counter=$(($counter+1))
+
 	fi
 }
 function winlostDays()
 {
+
 	for dayy in ${!daywiseAmountTrack[@]}
 	do
 		if [ ${daywiseAmountTrack[$dayy]} -ge $leastAllowance ]
@@ -60,10 +72,26 @@ function winlostDays()
 			fi
 	done
 }
+function luckyunlucky() 
+{
+
+	for dayy in ${!daywiseGrowthTrack[@]}
+	do
+	value=${daywiseGrowthTrack[$dayy]}
+		if [ $value -gt $maxAmount ]
+		then
+		maxAmount=$value
+		luckyday=$dayy
+	elif [ $value -lt $leastAmount ]
+			then
+			leastAmount=$value
+			unluckyday=$dayy
+		fi
+	done
+}
 
 
-
-function 20dayPlay() 
+function 20dayPlay()
 {
 
    for(( day=1; day <= $DAYS; day++ ))
@@ -74,12 +102,16 @@ function 20dayPlay()
          daywiseDictionary
    done
    winlostDays
+luckyunlucky
 }
 
 #bet
 #calculativeGame
 20dayPlay
-echo "Max total at the end of he day is=" $totalAmount
-echo "Days he won the bets= $dayswon"
-echo "Days he lost the bets= $dayslost"
-echo ${#daywiseAmountTrack[@]}
+echo "Max total at the end of he day is" $totalAmount
+echo $dayswon "days he won the bets"
+echo $dayslost "days he lost the bets"
+echo ${daywiseAmountTrack[@]}
+
+echo "unlucky day is" $unluckyday "with amount" $leastAmount
+echo "luckiest day is" $luckyday "with amount" $maxAmount
